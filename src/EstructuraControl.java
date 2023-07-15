2a2b4b023b503efcdb050ecfe1313bf0d7fc5b47
Milton Figueredo Miles <gratno@gmail.com>
2023-07-14 20:44:20 -0500

    Repasando

---
+++ src/EstructuraControl.java
@@ -0,0 +1,15 @@
+public class EstructuraControl {
+
+
+
+    public EstructuraControl(String[] args) {
+        int edad = 18;
+
+        if (edad >= 18) {
+            System.out.println("Eres mayor de edad puedes ingresar");
+        }
+    }
+}
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        int edad = 18;
+
+        if (edad >= 18) {
+            System.out.println("Eres mayor de edad puedes ingresar");
+        }
+
+        int numero =  7;
+
+        if (numero % 2 == 0) {
+            System.out.println("El númeroes par");
+         } else {
+            System.out.println("El número es impar");
+        }
+
+
+
+    }
+}
